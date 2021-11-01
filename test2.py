3945c210e9988e5514f33ce271e6b57ceeb4f628
Ethan <emraby21@gmail.com>
2021-11-01 10:53:04 -0400

    Added an assertion test

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import os
 # TEST CASE 1
-os.system("sortme.py -r")
+os.system("python3 sortme.py -r")
 print("Test Case 1: Checking the reversed sort...")
 with open('Sorted Reverse Text.txt') as f1, open('Output Reverse Text.txt') as f2:
     expected = f1.readlines()
@@ -8,7 +8,7 @@ with open('Sorted Reverse Text.txt') as f1, open('Output Reverse Text.txt') as f
     assert expected == actual
 
 #TEST CASE 2
-os.system("sortme.py")
+os.system("python3 sortme.py")
 print("Test Case 2: Checking the normal sort...")
 with open('Sorted Text.txt') as f1, open('Output Text.txt') as f2:
     expected = f1.readlines()
